1e85947e4082eba3e776a760c0d956f727b0a709
mossurtt <mossurtt@gmail.com>
2023-11-22 14:15:33 +0100

    edit-eslint-file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,47 +1,48 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    "extends": [
-        "airbnb",
-        "airbnb-typescript",
-        "airbnb/hooks",
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": './tsconfig.json'
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "ignorePatterns": ["vite.config.ts", "index.css", "tailwind.config.js", "postcss.config.js"],
-    "rules": {
-        "@typescript-eslint/no-shadow": "off",
-        "react/jsx-uses-react": "off",
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-props-no-spreading": "off",
-        "react/no-array-index-key": "off",
-        "import/prefer-default-export": "off",
-        "react/destructuring-assignment": "off",
-    }
-}
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+  ],
+  ignorePatterns: ['vite.config.ts', 'index.css', 'tailwind.config.js', 'postcss.config.js'],
+  rules: {
+    '@typescript-eslint/no-shadow': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-array-index-key': 'off',
+    'import/prefer-default-export': 'off',
+    'react/destructuring-assignment': 'off',
+    'no-console': 'off',
+  },
+};
